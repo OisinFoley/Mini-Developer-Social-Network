d62aa3e93f49c9e4c0f349ec264c34a6e5008a4c
oisinfoley <oisinfoley@yahoo.co.uk>
2018-08-22 22:36:37 +0200

    Created POST request for posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,14 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
+
+const validatePostInput = require('../../validation/post');
 
 // @route GET api/posts/test
 // @desc tests posts route
 // @access Public
 
-router.get("/test", (req, res) =>
+router.get('/test', (req, res) =>
   res.json({
-    message: "Posts works!"
+    message: 'Posts works!'
   })
+);
+
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(404).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      user: req.user.id,
+      avatar: req.body.avatar
+    });
+
+    newPost.save().then(post => res.json({ message: 'Success', post: post }));
+  }
 );
 
 module.exports = router;
