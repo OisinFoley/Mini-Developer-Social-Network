b6f5182f99e1df5344a2baa73de56ead1ba34a58
oisinfoley <oisinfoley@yahoo.co.uk>
2018-08-26 21:13:47 +0200

    Added GET all posts route, GET individual post route, DELETE individual post route.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 const validatePostInput = require('../../validation/post');
 
@@ -16,6 +17,60 @@ router.get('/test', (req, res) =>
     message: 'Posts works!'
   })
 );
+
+// @route GET api/posts/
+// @desc retrieve posts
+// @access Public
+
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => res.status(404).json({ noPosts: 'No posts found!' }));
+});
+
+// @route GET api/posts/
+// @desc retrieve posts
+// @access Public
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err =>
+      res.status(404).json({ noPost: 'No post with that id found!' })
+    );
+});
+
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+              unauthorised: 'User not authorised!'
+            });
+          }
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err =>
+          res
+            .status(404)
+            .json({ noPost: 'Post not found, no delete occurred!' })
+        );
+    });
+  }
+);
+
+// @route POST api/posts/
+// @desc add a post
+// @access Private
 
 router.post(
   '/',
