fdc08fd438c74b2268be37b07ed5822c5063ec3f
oisinfoley <oisinfoley@yahoo.co.uk>
2018-09-02 17:00:25 +0200

    Added redux logic for logging in and setting current user. Added logic to check for existance of token on reload, and then updating default isAuthenticated state with user info (default isAuthenticated state is falsy but now truth if token exists).

---
+++ client/src/utils/setTokenAsHeader.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const setTokenAsHeader = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export default setTokenAsHeader;
