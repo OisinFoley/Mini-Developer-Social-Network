54152d8aa404bfe60b36f929318dbf803f594cbf
oisinfoley <oisinfoley@yahoo.co.uk>
2018-09-08 22:27:27 +0200

    Added edit profile component route

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="mg-dark text-white mt-5 p-4 text-center">
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} DevConnector
     </footer>
   );
