f0afd0c4e403ba6dee7a4d98a79b9f04a8b2d6b4
oisinfoley <oisinfoley@yahoo.co.uk>
2018-09-12 23:00:25 +0200

    Populated profileAbout and profileCreds components with data from the user's profile and then nested those components inside of the main profile component.

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,95 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import isEmpty from '../../validation/is-empty';
 
 class ProfileAbout extends Component {
   render() {
     const { profile } = this.props;
 
+    const firstName = profile.user.name.trim().split(' ')[0];
+
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
     return (
       <div className="row">
         <div className="col-md-12">
-          <div className="card card-body bg-info text-white mb-3">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">
+              {firstName}
+              's Bio
+            </h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span> at {firstName} does not have a bio </span>
+              ) : (
+                <span> at {profile.bio} </span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
-              <div className="col-4 col-md-3 m-auto">
-                <img
-                  className="rounded-circle"
-                  src={profile.user.avatar}
-                  alt=""
-                />
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
               </div>
-            </div>
-            <div className="text-center">
-              <h1 className="display-4 text-center">{profile.user.name}</h1>
-              <p className="lead text-center">
-                {profile.status}
-                {isEmpty(profile.company) ? null : (
-                  <span> at {profile.company} </span>
-                )}
-              </p>
-              {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
-              <p>
-                {isEmpty(profile.website) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.website}
-                    target="_blank"
-                  >
-                    <i className="fab fa-globe fa-2x" />
-                  </a>
-                )}
-
-                {isEmpty(profile.social && profile.social[0].twitter) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.social[0].twitter}
-                    target="_blank"
-                  >
-                    <i className="fab fa-twitter fa-2x" />
-                  </a>
-                )}
-
-                {isEmpty(
-                  profile.social && profile.social[0].linkedin
-                ) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.social[0].linkedin}
-                    target="_blank"
-                  >
-                    <i className="fab fa-linkedin fa-2x" />
-                  </a>
-                )}
-
-                {isEmpty(profile.social && profile.social.instagram) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.social.instagram}
-                    target="_blank"
-                  >
-                    <i className="fab fa-instagram fa-2x" />
-                  </a>
-                )}
-
-                {isEmpty(profile.social && profile.social.facebook) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.social.facebook}
-                    target="_blank"
-                  >
-                    <i className="fab fa-facebook fa-2x" />
-                  </a>
-                )}
-
-                {isEmpty(profile.social && profile.social.youtube) ? null : (
-                  <a
-                    className="text-white p-2"
-                    href={profile.social.youtube}
-                    target="_blank"
-                  >
-                    <i className="fab fa-youtube fa-2x" />
-                  </a>
-                )}
-              </p>
             </div>
           </div>
         </div>
