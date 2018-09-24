e2c1e4d555c6e0ff8c872eebe883bb6d4a7b8a0b
oisinfoley <oisinfoley@yahoo.co.uk>
2018-09-24 20:55:38 +0200

    Updated .gitignore file.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 let app = express();
 
@@ -14,24 +15,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // db config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // connect to mongodb
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDb connected."))
+  .then(() => console.log('MongoDb connected.'))
   .catch(err => console.log(err));
 
 // passport middleware
 app.use(passport.initialize());
 
 // passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
