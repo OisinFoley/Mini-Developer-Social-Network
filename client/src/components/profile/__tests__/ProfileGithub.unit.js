013b402b034870958bd98bcdba5599b1cb5b4a82
oisinfoley <oisinfoley@yahoo.co.uk>
2019-07-30 20:48:45 +0200

    Adding tests for ProfileGithub component

--- client/src/components/profile/__tests__/ProfileGithub.unit.js
+++ client/src/components/profile/__tests__/ProfileGithub.unit.js
@@ -1,64 +1,49 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import ProfileGithub from '../ProfileGithub';
-// import { githubInfo } from '../../../__mocks__/mockGithubInfo';
+import React from 'react';
+import { mount } from 'enzyme';
+import ProfileGithub from '../ProfileGithub';
+import { githubInfo } from '../../../__mocks__/mockGithubInfo';
 
-// // const profile = mockProfiles[0];
-// // const mockProfileState = {
-// //   profile
-// // };
-// // const mockProfileWithoutBioState = {
-// //   profile: {
-// //     ...mockProfileState.profile,
-// //     bio: null
-// //   }
-// // }
+const props = {
+  username: 'test_username',
+  repos: []
+};
 
-// const props = {
-//   username: 'test_username',
-//   repos: []
-// };
+let wrapper;
+let mockSuccessResponse;
+let mockJsonPromise;
+let mockFetchPromise;
+beforeEach(() => {
+  mockSuccessResponse = githubInfo;
+  mockJsonPromise = Promise.resolve(mockSuccessResponse);
+  mockFetchPromise = Promise.resolve({
+    json: () => mockJsonPromise,
+  });
+  jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+});
 
-// let wrapper;
-// beforeEach(() => {
-//   wrapper = shallow(<ProfileGithub {...props} />);
-// });
-
-// describe('<ProfileGithub />', () => {
-//   it("shallow renders ProfileGithub, and fetches the repo data, then it displays the data", done => {
-//     const mockSuccessResponse = {githubInfo};
-//     const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
-//     const mockFetchPromise = Promise.resolve({ // 3
-//       json: () => mockJsonPromise,
-//     });
-//     jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise); // 4
-    
-//     const wrapper = shallow(<ProfileGithub />); // 5
+describe('<ProfileGithub />', () => {
+  it("mounts ProfileGithub, and fetches the repo data, then repos state matches the data returned from the mocked api call", done => {
+    const wrapper = mount(<ProfileGithub {...props} />);
                             
-//     expect(global.fetch).toHaveBeenCalledTimes(1);
-//     // expect(global.fetch).toHaveBeenCalledWith('https://url-of-your-server.com/example/json');
+    process.nextTick(() => {
+      expect(wrapper.state().repos).toEqual(githubInfo);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
 
-//     console.log(wrapper.state());
+      global.fetch.mockClear();
+      done();
+    });
+  });
 
-//     process.nextTick(() => { // 6
-//       // expect(wrapper.state().repos).toEqual(githubInfo);
+  mockSuccessResponse = [];
+  it(`mounts ProfileGithub, and when no repo data is returned from api call, then it shows text saying 'No repos to display' `, () => {
+    const wrapper = mount(<ProfileGithub {...props} />);
 
-//       global.fetch.mockClear(); // 7
-//       done(); // 8
-//     });
-  
-    
-//     // const mainHeaderText = wrapper.find('h3').at(0).text();
-//     // const usersBioParagraphText = wrapper.find('p').at(0).text().trim();
-
-//     // expect(mainHeaderText).toEqual(`${profile.user.name}'s Bio`);
-//     // expect(usersBioParagraphText).toEqual(`${profile.bio}`);
-//   });
-
-//   // it(`shallow renders ProfileGithub, and when bio isn't provided, then it shows {username} does not have a bio `, () => {
-//   //   wrapper = shallow(<ProfileGithub {...mockProfileWithoutBioState} />);
-//   //   const usersBioParagraphText = wrapper.find('p').at(0).text().trim();
-
-//   //   expect(usersBioParagraphText).toEqual(`${profile.user.name} does not have a bio`);
-//   // });
-// })
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(
+      wrapper.find('#profile-github__info-container')
+        .text()
+        .includes('No repos to display for this username')
+    )
+    .toEqual(true);
+  });
+});
