859fac58a7d84211028dfccd082832f549741417
oisinfoley <oisinfoley@yahoo.co.uk>
2019-08-06 14:10:37 +0200

    Initial refactoring, committing in order to revert and debug an issue in the client. May be related to authentication.

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+// import routes from './routes/index';
+
+
+let app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// db config
+// const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+// mongoose
+//   .connect(db)
+//   .then(() => console.log('MongoDb connected.'))
+//   .catch(err => console.log(err));
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require('./config/passport')(passport);
+
+// use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
