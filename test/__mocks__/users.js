4f5e1176962aefc352ac16e68e4a9adb5915de66
oisinfoley <oisinfoley@yahoo.co.uk>
2019-08-07 18:48:07 +0200

    Added tests for the api/users/ endpoints (/register and /login, which also covered the register and login validators' code branches.

---
+++ test/__mocks__/users.js
@@ -0,0 +1,9 @@
+const users = [{
+  name: 'test_user',
+  email: 'test_email@test.com',
+  password: 'test_password',
+  password2: 'test_password',
+  avatar: 'https://gravatar.com/test_avatar'
+}];
+
+module.exports = users;
