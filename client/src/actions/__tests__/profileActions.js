51a712f8eef942b3a0959501dd955f814bd61ab1
Oisin Foley <oisinfoley@yahoo.co.uk>
2019-08-24 18:24:36 +0200

    Updated auth, post and profile actions (as well as their test files), so that we're NOT using async await. Prior to this change, we were not correctly using async await (we were acting on the return of a promis eby using then()), hence the syntax is misleading at this time. (#11)

--- client/src/actions/__tests__/profileActions.js
+++ client/src/actions/__tests__/profileActions.js
@@ -29,86 +29,104 @@ describe('profileActions: mocking history', () => {
   });
 
   describe('createProfile', () => {
-    it(`should create profile by calling '/api/profiles', then push /dashboard to history`, async () => {
-      await actions.createProfile('the profile data', history)(dispatch);
-      expect(mock).toHaveBeenCalledWith('/api/profiles', 'the profile data');
-      expect(history.push).toHaveBeenCalledWith('/dashboard');
+    it(`should create profile by calling '/api/profiles', then push /dashboard to history`, () => {
+      actions.createProfile('the profile data', history)(dispatch)
+        .then(() => {
+          expect(mock).toHaveBeenCalledWith('/api/profiles', 'the profile data');
+          expect(history.push).toHaveBeenCalledWith('/dashboard');
+        });
     });
   });
 
   describe('addEducation', () => {
-    it('should create education then push /dashboard to history', async () => {
-      await actions.addEducation('the education data', history)(dispatch);
-      expect(mock).toHaveBeenCalledWith('/api/profiles/education', 'the education data');
-      expect(history.push).toHaveBeenCalledWith('/dashboard');
+    it('should create education then push /dashboard to history', () => {
+      actions.addEducation('the education data', history)(dispatch)
+        .then(() => {
+          expect(mock).toHaveBeenCalledWith('/api/profiles/education', 'the education data');
+          expect(history.push).toHaveBeenCalledWith('/dashboard');
+        });
     });
   });
 
   describe('addExperience', () => {
-    it('should create education then push /dashboard to history', async () => {
-      await actions.addExperience('the experience data', history)(dispatch);
-      expect(mock).toHaveBeenCalledWith('/api/profiles/experience', 'the experience data');
-      expect(history.push).toHaveBeenCalledWith('/dashboard');
+    it('should create education then push /dashboard to history', () => {
+      actions.addExperience('the experience data', history)(dispatch)
+        .then(() => {
+          expect(mock).toHaveBeenCalledWith('/api/profiles/experience', 'the experience data');
+          expect(history.push).toHaveBeenCalledWith('/dashboard');
+        });
     });
   });
 
 });
 
 describe('getCurrentProfile', () => {
-  it('dispatches setProfileLoading, then dispatches GET_PROFILE', async () => {
-    await store.dispatch(actions.getCurrentProfile());
-    expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING});
-    expect(storeActions[1]).toEqual({ type: types.GET_PROFILE, payload: {} });
+  it('dispatches setProfileLoading, then dispatches GET_PROFILE', () => {
+    return store.dispatch(actions.getCurrentProfile())
+      .then(() => {
+        expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING});
+        expect(storeActions[1]).toEqual({ type: types.GET_PROFILE, payload: {} });
+      });
   });
 });
 
 describe('deleteEducation', () => {
-  it('should delete education then dispatch GET_PROFILE', async () => {
-    await store.dispatch(actions.deleteEducation('edu123'));
-    expect(storeActions[0]).toEqual({ type: types.GET_PROFILE, payload: {}});
+  it('should delete education then dispatch GET_PROFILE', () => {
+    return store.dispatch(actions.deleteEducation('edu123'))
+      .then(() => {
+        expect(storeActions[0]).toEqual({ type: types.GET_PROFILE, payload: {}});
+      });
   });
 });
 
 describe('deleteExperience', () => {
-  it('should delete experience then dispatch GET_PROFILE', async () => {
-    await store.dispatch(actions.deleteExperience('exp123'));
-    expect(storeActions[0]).toEqual({ type: types.GET_PROFILE, payload: {}});
+  it('should delete experience then dispatch GET_PROFILE', () => {
+    return store.dispatch(actions.deleteExperience('exp123'))
+      .then(() => {
+        expect(storeActions[0]).toEqual({ type: types.GET_PROFILE, payload: {}});
+      });
   });
 });
 
 describe('getProfiles', () => {
-  it('should dispatch setProfileLoading then dispatch GET_PROFILES with data in the payload after successful callback from the endpoint', async () => {
-    await store.dispatch(actions.getProfiles());
-    expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING });
-    expect(storeActions[1]).toEqual({ type: types.GET_PROFILES, payload: ['profile1, profile2, profile3'] });
+  it('should dispatch setProfileLoading then dispatch GET_PROFILES with data in the payload after successful callback from the endpoint', () => {
+    return store.dispatch(actions.getProfiles())
+      .then(() => {
+        expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING });
+        expect(storeActions[1]).toEqual({ type: types.GET_PROFILES, payload: ['profile1, profile2, profile3'] });
+      });
   });
 });
 
 describe('getProfileByHandle', () => {
-  it('should dispatch setProfileLoading then dispatch GET_PROFILE with data in the payload after successful callback from the endpoint', async () => {
-    await store.dispatch(actions.getProfileByHandle('user123'));
-    expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING });
-    expect(storeActions[1]).toEqual({ type: types.GET_PROFILE, payload: 'fakeProfile' });
+  it('should dispatch setProfileLoading then dispatch GET_PROFILE with data in the payload after successful callback from the endpoint', () => {
+    return store.dispatch(actions.getProfileByHandle('user123'))
+      .then(() => {
+        expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING });
+        expect(storeActions[1]).toEqual({ type: types.GET_PROFILE, payload: 'fakeProfile' });
+      });
   });
 });
 
 describe('deleteAccount', () => {
-  it('should dispatch SET_CURRENT_USER with empty payload after successful callback from the endpoint', async () => {
-    await store.dispatch(actions.deleteAccount());
-    expect(storeActions[0]).toEqual({ type: types.SET_CURRENT_USER, payload: {} });
+  it('should dispatch SET_CURRENT_USER with empty payload after successful callback from the endpoint', () => {
+    return store.dispatch(actions.deleteAccount())
+    .then(() => {
+      expect(storeActions[0]).toEqual({ type: types.SET_CURRENT_USER, payload: {} });
+    });
   });
 });
 
 describe('setProfileLoading', () => {
-  it('should dispatch PROFILE_LOADING', async () => {
-    await store.dispatch(actions.setProfileLoading());
+  it('should dispatch PROFILE_LOADING', () => {
+    store.dispatch(actions.setProfileLoading());
     expect(storeActions[0]).toEqual({ type: types.PROFILE_LOADING });
   });
 });
 
 describe('clearCurrentProfile', () => {
-  it('should dispatch CLEAR_CURRENT_PROFILE', async () => {
-    await store.dispatch(actions.clearCurrentProfile());
+  it('should dispatch CLEAR_CURRENT_PROFILE', () => {
+    store.dispatch(actions.clearCurrentProfile());
     expect(storeActions[0]).toEqual({ type: types.CLEAR_CURRENT_PROFILE });
   });
 });
