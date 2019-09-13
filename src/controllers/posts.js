bbcd93a1fe6c68290b8a55660be098a316247e3e
Oisin Foley <oisinfoley@yahoo.co.uk>
2019-09-12 17:27:30 -0700

    Added error handling middlware to src/app.js where we now use a util function to decide the response code based on the err type. This allows us to neatly send a 500 code for any err that we don't know how to handle. This means that most errors in our controllers are passed along the pipelineusing 'next()'. Also, am passing the error strings file into the request validator files when calling them, rather than importingthe error strings within the validation files themselves. We're now logging the errors that're caught by the middleware, so that we can look back on what happened later on, if debugging an issue is proving to be difficult.  Moved some util functions that're used during testing from src/test/utils to src/utils. (#14)

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,6 +1,6 @@
 import PostsService from '../services/posts';
 import validatePostInput from '../validation/post';
-import errorMessages from '../error-handling/strings';
+import errorMessages from '../utils/error-handling-strings';
 
 class PostsController {
   getAllPosts (req, res) {
@@ -8,88 +8,69 @@ class PostsController {
       .then(posts => res.json(posts));
   };
 
-  getSinglePost (req, res) {
+  getSinglePost (req, res, next)  {
     const { id } = req.params;
     PostsService.getPost(id, errorMessages)
       .then(post => res.json(post))
-      .catch(err => {
-        if (err.postNotFound) res.status(404).json(err);
-      });
+      .catch(err => next(err));
   };
 
-  deleteSinglePost (req, res) {
+  deleteSinglePost (req, res, next) {
     const { id } = req.params;
     const userId = req.user.id;
 
     PostsService.deletePost(id, userId, errorMessages)
       .then(() => res.status(204).json())
-      .catch(err => {
-        if (err.unauthorised) res.status(401).json(err);
-        if (err.postNotFound) res.status(404).json(err);
-      });
+      .catch(err => next(err));
   };
 
-  addNewPost (req, res) {
-    const { errors, isValid } = validatePostInput(req.body);
+  addNewPost (req, res, next) {
+    const { errors, isValid } = validatePostInput(req.body, errorMessages);
     const postData = {...req.body, user: req.user.id };
 
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+    if (!isValid)
+      next(errors);
 
     PostsService.addPost(postData)
       .then(post => res.status(201).json(post));
   };
 
-  addLikeToPost (req, res) {
+  addLikeToPost (req, res, next) {
     const { id } = req.params;
     const userId = req.user.id;
 
     PostsService.addLike(id, userId, errorMessages)
       .then(post => res.status(201).json(post))
-      .catch(err => {
-        if (err.likedAlready) res.status(400).json(err);
-        if (err.postNotFound) res.status(404).json(err);
-      });
+      .catch(err => next(err));
   };
 
-  removeLikeFromPost (req, res) {
+  removeLikeFromPost (req, res, next) {
     const { id } = req.params;
     const userId = req.user.id;
 
     PostsService.removeLike(id, userId, errorMessages)
       .then(post => res.json(post))
-      .catch(err => {
-        if (err.cannotUnlike) res.status(400).json(err);
-        if (err.postNotFound) res.status(404).json(err);
-      });
+      .catch(err => next(err));
   };
 
-  addCommentToPost (req, res) {
-    const { errors, isValid } = validatePostInput(req.body);
+  addCommentToPost (req, res, next) {
+    const { errors, isValid } = validatePostInput(req.body, errorMessages);
     const commentData = {...req.body, user: req.user.id };
     const { id } = req.params;
 
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+    if (!isValid)
+      next(errors);
   
     PostsService.addComment(id, commentData, errorMessages)
       .then(post => res.status(201).json(post))
-      .catch(err => {
-        if (err.postNotFound) res.status(404).json(err);
-      });
+      .catch(err => next(err));
   };
 
-  deleteCommentFromPost (req, res) {
+  deleteCommentFromPost (req, res, next) {
     const { post_id, comment_id } = req.params;
     PostsService.deleteComment(post_id, comment_id, errorMessages)
       .then(post => res.json(post))
-      .catch(err => {
-        if (err.postNotFound || err.commentNotFound) {
-          res.status(404).json(err);
-        }
-      });
+      .catch(err => next(err));
   };
 };
 
